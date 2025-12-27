a8ccdb5553037eaaa31e0ecea1222b34211997e8
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-12-27 22:45:23 +0600

    add my bookings page

---
+++ src/app/(dashboardLayout)/(userDashboardLayout)/dashboard/my-bookings/page.tsx
@@ -0,0 +1,44 @@
+// app/admin/dashboard/tour-management/page.tsx
+import TourFilters from "@/components/modules/Admin/TourManagement/TourFilters";
+import { TableSkeleton } from "@/components/shared/TableSkeleton";
+import { Suspense } from "react";
+import { getAllTours, getAllTourTypes } from "@/services/admin/tourManagement";
+import ToursTable from "@/components/modules/Admin/TourManagement/tourTable";
+import { getAllDivisions } from "@/services/admin/divisionsManagement";
+import { queryStringFormatter } from "@/lib/formatters";
+import TourManagementHeader from "@/components/modules/Admin/TourManagement/toursManagementHeader";
+
+const MyBookingsPage = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) => {
+  const searchParamsObj = await searchParams;
+  const queryString = queryStringFormatter(searchParamsObj);
+
+  const toursRes = await getAllTours(queryString);
+  const divisionsRes = await getAllDivisions();
+  const tourTypesRes = await getAllTourTypes();
+
+  const tours = toursRes?.data ?? [];
+  const divisions = divisionsRes?.data ?? [];
+  const tourTypes = tourTypesRes?.data ?? [];
+
+
+
+  return (
+    <div className="space-y-6">
+      <TourManagementHeader
+        divisions={divisions}
+        tourTypes={tourTypes}
+      />
+      <TourFilters divisions={divisions} tourTypes={tourTypes} />
+      
+      <Suspense fallback={<TableSkeleton columns={5} rows={10} />}>
+        <ToursTable tours={tours} divisions={divisions} tourTypes={tourTypes} />
+      </Suspense>
+    </div>
+  );
+};
+
+export default MyBookingsPage;
